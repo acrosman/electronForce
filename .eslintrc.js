24fab340b9809833f5b281cd00f86308e7357930
Aaron Crosman <acrosman@gmail.com>
2020-03-20 23:35:06 -0400

    Actual .eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "ignorePatterns": [
+        "temp.js",
+        "node_modules/",
+        ".vscode/",
+        ".sfdx/",
+    ],
+    "rules": {
+        "no-console": 0,
+        "no-unused-vars": 1,
+    }
+};
