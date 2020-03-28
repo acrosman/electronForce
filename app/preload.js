2a88b0be138ecda078b1ae08ca6e68dff0fc1a54
Aaron Crosman <acrosman@gmail.com>
2020-03-28 15:04:19 -0400

    Refactored interface for better layout.

--- app/preload.js
+++ app/preload.js
@@ -1,20 +1,9 @@
 // Preload script.
-const { contextBridge, ipcRenderer } = require('electron');
-
-document.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-
-  ['chrome', 'node', 'electron'].forEach((type) => {
-    replaceText(`${type}-version`, process.versions[type]);
-  });
-});
+const { contextBridge, ipcRenderer, remote } = require('electron');  // eslint-disable-line
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object.
-// Big hattip:https://stackoverflow.com/a/59814127/24215.
+// Big hat tip: https://stackoverflow.com/a/59814127/24215.
 contextBridge.exposeInMainWorld(
   'api', {
     send: (channel, data) => {
--- app/render.js
+++ app/render.js
@@ -1,3 +1,65 @@
+/* global $ */
+// Initial interface setup using jQuery (since it's around from bootstrap anyway).
+$(document).ready(() => {
+  // Hide the places for handling responses until we have some.
+  $('#org-status').hide();
+  $('#api-request-form').hide();
+  $('#results-table-wrapper').hide();
+
+  // Setup to show/hide all the various controls needed for the APIs.
+  // Initially this is deeply insufficient, when enough controls exist this code
+  // style will be really really unmaintainable.
+  // @TODO: Do this better!
+  const apiSelectors = {
+    'rest-api-soql': 'query',
+    'rest-api-sosl': 'search',
+    'rest-api-crud': undefined,
+    'rest-api-describe': undefined,
+    'rest-api-apex': undefined,
+    'analytics-api': undefined,
+    'bulk-api': undefined,
+    'chatter-api': undefined,
+    'metadata-api': undefined,
+    'streaming-api': undefined,
+    'tooling-api': undefined,
+  };
+
+  let element;
+  Object.keys(apiSelectors).forEach((selector) => {
+    element = $(`#${selector}`);
+    if (element) {
+      element.hide();
+      const trigger = $('.sf-api-trigger-button', element);
+      trigger.wrapperElement = element;
+      // This click handler provides the trigger to send messages to the main process.
+      trigger.click((event) => {
+        const dataElements = $('.api-data-element', event.currentTarget.wrapperElement);
+        const data = { org: $('#active-org').val() };
+        dataElements.each((index, item) => {
+          data[$(item).attr('id').replace(/-/g, '_')] = $(item).val();
+        });
+        // Send prepared data to the main process.
+        window.api.send(`sf_${apiSelectors[selector]}`, data);
+      });
+    }
+  });
+
+  $('#select-api').change(() => {
+    // Show the controls for the selected API.
+    const newValue = $('#select-api').val();
+    $(`#${newValue}`).show();
+
+    // Hide all other controls.
+    let hideSelector;
+    $('#select-api').find('option').each((index, item) => {
+      if (item.value !== newValue) {
+        hideSelector = item.value;
+        $(`#${hideSelector}`).hide();
+      }
+    });
+  });
+});
+
 // ============= Helpers ==============
 const replaceText = (selector, text) => {
   const element = document.getElementById(selector);
@@ -131,61 +193,4 @@ document.getElementById('logout-trigger').addEventListener('click', () => {
   document.getElementById('org-status').style.display = 'none';
   // @TODO: Remove org from list of active orgs.
   // @TODO: Update/hide status area if no orgs remain.
-});
-
-
-// ================== Inital page setup =====================
-
-// Hide the information for handling responses.
-document.getElementById('org-status').style.display = 'none';
-document.getElementById('api-request-form').style.display = 'none';
-document.getElementById('results-table-wrapper').style.display = 'none';
-
-
-// Setup to show/hide all the various controls needed for the APIs.
-// Initially this is deeply insufficient, when enough controls exist this code
-// style will be really really unmaintainable.
-// @TODO: Do this better!
-const apiSelectors = {
-  'rest-api-soql': 'query',
-  'rest-api-sosl': 'search',
-  'rest-api-crud': undefined,
-  'rest-api-describe': undefined,
-  'rest-api-apex': undefined,
-  'analytics-api': undefined,
-  'bulk-api': undefined,
-  'chatter-api': undefined,
-  'metadata-api': undefined,
-  'streaming-api': undefined,
-  'tooling-api': undefined,
-};
-
-let element;
-Object.keys(apiSelectors).forEach((selector) => {
-  element = document.getElementById(selector);
-  if (element) {
-    element.style.display = 'none';
-    const trigger = element.getElementsByClassName('sf-api-trigger-button')[0];
-    trigger.wrapperElement = element;
-    trigger.addEventListener('click', (event) => {
-      const dataElements = event.currentTarget.wrapperElement.getElementsByClassName('api-data-element');
-      const data = { org: document.getElementById('active-org').value };
-      for (let i = 0; i < dataElements.length; i += 1) {
-        data[dataElements[i].id.replace(/-/g, '_')] = dataElements[i].value;
-      }
-      window.api.send(`sf_${apiSelectors[selector]}`, data);
-    });
-  }
-});
-
-document.getElementById('select-api').addEventListener('change', () => {
-  const selectElement = document.getElementById('select-api');
-  const newValue = selectElement.value;
-
-  document.getElementById(newValue).style.display = 'block';
-  for (let i = 0; i < selectElement.options.length; i += 1) {
-    if (selectElement.options[i].value !== newValue) {
-      document.getElementById(selectElement.options[i].value).style.display = 'none';
-    }
-  }
 });
