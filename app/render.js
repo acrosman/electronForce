10a4914f414ced35e4fc595a3fe61a695d0eceb7
Aaron Crosman <acrosman@gmail.com>
2020-05-31 12:48:02 -0400

    Switch global describe to table display.

--- app/render.js
+++ app/render.js
@@ -56,10 +56,34 @@ $(document).ready(() => {
 });
 
 // ============= Helpers ==============
+// Simple find and replace of text based on selector.
 const replaceText = (selector, text) => {
   const element = document.getElementById(selector);
   if (element) element.innerText = text;
 };
+
+// Convert a simple object with name/value pairs, and sub-objects into an Unordered list
+const object2ul = (data) => {
+  const ul = document.createElement('ul');
+  const keys = Object.keys(data);
+  let li;
+  let i;
+
+  for (i = 0; i < keys.length; i += 1) {
+    li = document.createElement('li');
+    // if it's sub-object recurse.
+    if (typeof data[keys[i]] === 'object' && data[keys[i]] !== null) {
+      li.appendChild(object2ul(data[keys[i]]));
+    } else {
+      // append the text to the li.
+      li.appendChild(document.createTextNode(data[keys[i]]));
+    }
+    ul.appendChild(li); // append the list item to the ul
+  }
+
+  return ul;
+};
+
 
 const displayRawResponse = (responseObject) => {
   replaceText('raw-response', JSON.stringify(responseObject, undefined, 2));
@@ -145,6 +169,64 @@ const refreshObjectDisplay = (data) => {
   });
 };
 
+const displayGlobalDescribe = (sObjectData) => {
+  // Setup.
+  document.getElementById('results-table-wrapper').style.display = 'block';
+  document.getElementById('results-object-viewer-wrapper').style.display = 'none';
+  document.getElementById('results-summary-count').innerText = `Your orgs contains ${sObjectData.length} objects (custom and standard)`;
+
+  // Get the table.
+  const resultsTable = document.querySelector('#results-table');
+
+  // Clear existing table.
+  while (resultsTable.firstChild) {
+    resultsTable.removeChild(resultsTable.firstChild);
+  }
+
+  // Extract the header.
+  const keys = Object.keys(sObjectData[0]);
+
+  // Create the header row for the table.
+  const tHead = document.createElement('thead');
+  const headRow = document.createElement('tr');
+  headRow.setAttribute('class', 'table-primary');
+  let newHeader;
+  let contentNode;
+  // Add the other columns from the result set.
+  for (let i = 0; i < keys.length; i += 1) {
+    newHeader = document.createElement('th');
+    newHeader.setAttribute('scope', 'col');
+    contentNode = document.createTextNode(keys[i]);
+    newHeader.appendChild(contentNode);
+    headRow.appendChild(newHeader);
+  }
+  tHead.appendChild(headRow);
+  resultsTable.appendChild(tHead);
+
+  // Add the data.
+  let dataRow;
+  let newData;
+  const tBody = document.createElement('tbody');
+  for (let i = 0; i < sObjectData.length; i += 1) {
+    dataRow = document.createElement('tr');
+    for (let j = 0; j < keys.length; j += 1) {
+      // Special Case handling for urls field
+      if (keys[j] === 'urls') {
+        contentNode = object2ul(sObjectData[i][keys[j]]);
+      } else {
+        contentNode = document.createTextNode(sObjectData[i][keys[j]]);
+      }
+
+      // Add the new content to the row
+      newData = document.createElement('td');
+      newData.appendChild(contentNode);
+      dataRow.appendChild(newData);
+    }
+    tBody.appendChild(dataRow);
+  }
+  resultsTable.appendChild(tBody);
+};
+
 // ===== Response handlers from IPC Messages to render context ======
 // Login response.
 window.api.receive('response_login', (data) => {
@@ -203,7 +285,7 @@ window.api.receive('response_describe_global', (data) => {
   document.getElementById('results-object-viewer-wrapper').style.display = 'block';
   displayRawResponse(data);
   if (data.status) {
-    refreshObjectDisplay(data);
+    displayGlobalDescribe(data.response.sobjects);
   }
 });
 
