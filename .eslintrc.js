c4dbc065226388adec4ed9f64ec3d39efdb6afb9
sabina <sabina.zaripova@nu.edu.kz>
2020-10-11 13:24:14 +0600

    fix: change .eslintrc.js rules to the original one

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
-module.exports={
-    "extends" : "airbnb-base"
-   }
-   
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-unused-vars": 1,
+        "no-param-reassign": ["error", { "props": false }]
+    }
+};
