a71fec5028d62e211fe6185d32e47d36071657c1
Aaron Crosman <acrosman@gmail.com>
2025-05-09 18:20:09 -0400

    Updates to electron and JSForce

--- src/electronForce.js
+++ src/electronForce.js
@@ -30,42 +30,29 @@ const handlers = {
     });
   },
   // Login to an org using password authentication.
-  sf_login: (event, args) => {
+  sf_login: async (event, args) => {
     const conn = new jsforce.Connection({
-      // you can change loginUrl to connect to sandbox or prerelease env.
       loginUrl: args.url,
     });
 
-    let { password } = args;
-    if (args.token !== '') {
-      password = `${password}${args.token}`;
-    }
+    try {
+      let { password } = args;
+      if (args.token !== '') {
+        password = `${password}${args.token}`;
+      }
 
-    conn.login(args.username, password, (err, userInfo) => {
-      // Since we send the args back to the interface, it's a good idea
-      // to remove the security information.
+      // Clear sensitive data
       args.password = '';
       args.token = '';
 
-      if (err) {
-        logMessage('Error', `Login Failed ${err}`);
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Login Failed',
-          response: err,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
-        return true;
-      }
-      // Save the next connection in the global storage.
+      const userInfo = await conn.login(args.username, password);
+
       sfConnections[userInfo.organizationId] = {
         instanceUrl: conn.instanceUrl,
         accessToken: conn.accessToken,
         version: '51.0',
       };
 
-      // Record the connection in the log.
       logMessage('Info', `Connection Org ${userInfo.organizationId} for User ${userInfo.id}`);
 
       mainWindow.webContents.send('response_login', {
@@ -75,27 +62,25 @@ const handlers = {
         limitInfo: conn.limitInfo,
         request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Login Failed ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Login Failed',
+        response: err,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Logout of a specific Salesforce org.
-  sf_logout: (event, args) => {
-    const conn = new jsforce.Connection(sfConnections[args.org]);
-    conn.logout((err) => {
-      if (err) {
-        mainWindow.webContents.send('response_logout', {
-          status: false,
-          message: 'Logout Failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
-        logMessage('Error', `Logout Failed ${err}`);
-        return true;
-      }
-      logMessage('Info', `Logged out of ${args.org}`);
 
-      // now the session has been expired.
+  // Logout of Salesforce.
+  sf_logout: async (event, args) => {
+    const conn = new jsforce.Connection(sfConnections[args.org]);
+    try {
+      await conn.logout();
+
+      logMessage('Info', `Logged out of ${args.org}`);
       mainWindow.webContents.send('response_logout', {
         status: true,
         message: 'Logout Successful',
@@ -104,28 +89,26 @@ const handlers = {
         request: args,
       });
       sfConnections[args.org] = null;
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Logout Failed ${err}`);
+      mainWindow.webContents.send('response_logout', {
+        status: false,
+        message: 'Logout Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Run a SOQL query against an org.
-  sf_query: (event, args) => {
+
+  // Run a SOQL Query against your org.
+  sf_query: async (event, args) => {
     const conn = new jsforce.Connection(sfConnections[args.org]);
-    conn.query(args.rest_api_soql_text, (err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Query Failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
-        logMessage('Error', `Query Failed ${err}`);
-        return true;
-      }
+    try {
+      const result = await conn.query(args.rest_api_soql_text);
 
       logMessage('Info', `Ran Query: ${args.rest_api_soql_text}`);
 
-      // Send records back to the interface.
       mainWindow.webContents.send('response_query', {
         status: true,
         message: 'Query Successful',
@@ -133,34 +116,31 @@ const handlers = {
         limitInfo: conn.limitInfo,
         request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Query Failed ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Query Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Search Salesforce with a SOSL query.
-  sf_search: (event, args) => {
+
+  // Run SOSL search.
+  sf_search: async (event, args) => {
     const conn = new jsforce.Connection(sfConnections[args.org]);
-    conn.search(args.rest_api_sosl_text, (err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Search Failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
-        logMessage('Error', `Search Failed ${err}`);
-        return true;
-      }
+    try {
+      const result = await conn.search(args.rest_api_sosl_text);
 
       logMessage('Info', `Ran Search: ${args.rest_api_sosl_text}`);
 
-      // Re-package results for easy display.
       const adjustedResult = {
         records: result.searchRecords,
         totalSize: 'n/a',
       };
 
-      // Send records back to the interface.
       mainWindow.webContents.send('response_query', {
         status: true,
         message: 'Search Successful',
@@ -168,29 +148,26 @@ const handlers = {
         limitInfo: conn.limitInfo,
         request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Search Failed ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Search Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Describe a Salesforce object
-  sf_describe: (event, args) => {
-    const conn = new jsforce.Connection(sfConnections[args.org]);
-    conn.sobject(args.rest_api_describe_text).describe((err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Describe Failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
 
-        logMessage('Error', `Describe Failed ${err}`);
-        return true;
-      }
+  // Run an object describe.
+  sf_describe: async (event, args) => {
+    const conn = new jsforce.Connection(sfConnections[args.org]);
+    try {
+      const result = await conn.sobject(args.rest_api_describe_text).describe();
 
       logMessage('Info', `Describe of ${args.rest_api_describe_text} Successful`);
 
-      // Send records back to the interface.
       mainWindow.webContents.send('response_describe', {
         status: true,
         message: `Describe ${args.rest_api_describe_text} Successful`,
@@ -198,29 +175,26 @@ const handlers = {
         limitInfo: conn.limitInfo,
         request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Describe Failed ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Describe Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Run a Global Describe.
-  sf_describeGlobal: (event, args) => {
-    const conn = new jsforce.Connection(sfConnections[args.org]);
-    conn.describeGlobal((err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Describe Global Failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
 
-        logMessage('Error', `Global Describe Failed ${err}`);
-        return true;
-      }
+  // Run a Global Describe
+  sf_describeGlobal: async (event, args) => {
+    const conn = new jsforce.Connection(sfConnections[args.org]);
+    try {
+      const result = await conn.describeGlobal();
 
       logMessage('Info', 'Global Describe Successful');
 
-      // Send records back to the interface.
       mainWindow.webContents.send('response_describe_global', {
         status: true,
         message: 'Describe Global Successful',
@@ -228,90 +202,58 @@ const handlers = {
         limitInfo: conn.limitInfo,
         request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Global Describe Failed ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Describe Global Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Fetch the org object for specified org.
-  sf_orgExplore: (event, args) => {
+
+  // Fetch the Organization object from the active org.
+  sf_orgExplore: async (event, args) => {
     const conn = new jsforce.Connection(sfConnections[args.org]);
+    try {
+      const result = await conn.sobject('Organization').describe();
 
-    // Get the org object's list of fields.
-    conn.sobject('Organization').describe((err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Describe Org in fetch process failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
-
-        logMessage('Error', `Describe Org in fetch process failed ${err}`);
-        return true;
-      }
-
-      // Iterate over the list of fields and write a useful query.
-      let fields = '';
-      for (let i = 0; i < result.fields.length; i += 1) {
-        if (i === 0) {
-          fields = result.fields[i].name;
-        } else {
-          fields = `${fields}, ${result.fields[i].name}`;
-        }
-      }
-
+      const fields = result.fields.map((field) => field.name).join(', ');
       const orgQuery = `SELECT ${fields} FROM Organization`;
 
-      // Retreive all fields for this org's Organization object.
-      conn.query(orgQuery, (qErr, qResult) => {
-        if (qErr) {
-          mainWindow.webContents.send('response_generic', {
-            status: false,
-            message: 'Org Fetch Failed',
-            response: qErr,
-            limitInfo: conn.limitInfo,
-            request: args,
-          });
+      const qResult = await conn.query(orgQuery);
 
-          logMessage('Error', `Org Fetch Failed ${qErr}`);
-          return true;
-        }
+      logMessage('Info', `Fetched Org Details with ${orgQuery}`);
 
-        logMessage('Info', `Fetched Org Details with ${orgQuery}`);
-
-        // Send records back to the interface.
-        mainWindow.webContents.send('response_org_object_display', {
-          status: true,
-          message: 'Fetched Org Details',
-          response: qResult.records[0],
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
-        return true;
+      mainWindow.webContents.send('response_org_object_display', {
+        status: true,
+        message: 'Fetched Org Details',
+        response: qResult.records[0],
+        limitInfo: conn.limitInfo,
+        request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Org Fetch Failed ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Org Fetch Failed',
+        response: err,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Report on current org limits.
-  sf_orgLimits: (event, args) => {
-    const conn = new jsforce.Connection(sfConnections[args.org]);
-    conn.limits((err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Limits Check Failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
 
-        logMessage('Error', `Limit Check Error: ${err}`);
-        return true;
-      }
+  // Report current org limits
+  sf_orgLimits: async (event, args) => {
+    const conn = new jsforce.Connection(sfConnections[args.org]);
+    try {
+      const result = await conn.limits();
 
       logMessage('Info', 'Fetched Org limits');
 
-      // Send records back to the interface.
       mainWindow.webContents.send('response_org_limits', {
         status: true,
         message: 'Org Limit Check Successful',
@@ -319,30 +261,27 @@ const handlers = {
         limitInfo: conn.limitInfo,
         request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Limit Check Error: ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Limits Check Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Fetch list of org profiles
-  sf_orgProfiles: (event, args) => {
-    const profileQuery = 'SELECT Id, Name, Description FROM Profile';
-    const conn = new jsforce.Connection(sfConnections[args.org]);
-    conn.query(profileQuery, (err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Profile Listing Failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
 
-        logMessage('Error', `Profile Listing Error: ${err}`);
-        return true;
-      }
+  // List all profiles.
+  sf_orgProfiles: async (event, args) => {
+    const conn = new jsforce.Connection(sfConnections[args.org]);
+    try {
+      const profileQuery = 'SELECT Id, Name, Description FROM Profile';
+      const result = await conn.query(profileQuery);
 
       logMessage('Info', `Fetched Profile listing with: ${profileQuery}`);
 
-      // Send records back to the interface.
       mainWindow.webContents.send('response_query', {
         status: true,
         message: 'Profile Listing Complete',
@@ -350,30 +289,27 @@ const handlers = {
         limitInfo: conn.limitInfo,
         request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Profile Listing Error: ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Profile Listing Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Fetch org Permission Sets
-  sf_orgPermSets: (event, args) => {
-    const psQuery = 'SELECT Id, Name, Label, Description, IsCustom, IsOwnedByProfile, Profile.Name FROM PermissionSet ORDER BY IsOwnedByProfile, IsCustom DESC';
-    const conn = new jsforce.Connection(sfConnections[args.org]);
-    conn.query(psQuery, (err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'PermSet Listing Failed',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
 
-        logMessage('Error', `PermSet Listing Error: ${err}`);
-        return true;
-      }
+  // List all PermSets.
+  sf_orgPermSets: async (event, args) => {
+    const conn = new jsforce.Connection(sfConnections[args.org]);
+    try {
+      const psQuery = 'SELECT Id, Name, Label, Description, IsCustom, IsOwnedByProfile, Profile.Name FROM PermissionSet ORDER BY IsOwnedByProfile, IsCustom DESC';
+      const result = await conn.query(psQuery);
 
       logMessage('Info', `Fetched Permissions with: ${psQuery}`);
 
-      // Send records back to the interface.
       mainWindow.webContents.send('response_permset_list', {
         status: true,
         message: 'Permission Set Listing Complete',
@@ -381,31 +317,26 @@ const handlers = {
         limitInfo: conn.limitInfo,
         request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `PermSet Listing Error: ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'PermSet Listing Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
-  // Fetch org Permission Set Details
-  sf_orgPermSetDetail: (event, args) => {
+
+  // Fetch details about a permission set.
+  sf_orgPermSetDetail: async (event, args) => {
     const conn = new jsforce.Connection(sfConnections[args.org]);
-
-    // Get the details of the Permission Sets for this org, then query the requested set.
-    conn.sobject('PermissionSet').describe((err, result) => {
-      if (err) {
-        mainWindow.webContents.send('response_generic', {
-          status: false,
-          message: 'Profile Detail Lookup Failed: unable to describe permissions sets',
-          response: `${err}`,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
-
-        logMessage('Error', `Detail Lookup Failed on Describing Permission Sets: ${err}`);
-        return true;
-      }
+    try {
+      const result = await conn.sobject('PermissionSet').describe();
 
       logMessage('Info', 'Fetched Permission Set Describe');
 
-      // Built a unique list of field names to use in query in preferred order.
       const fieldNames = [
         'Id',
         'Name',
@@ -423,34 +354,27 @@ const handlers = {
 
       const permSetQuery = `SELECT ${fieldNames.join(', ')} FROM PermissionSet WHERE Name = '${args.org_permset_detail_name}' ORDER BY IsOwnedByProfile, IsCustom DESC`;
 
-      conn.query(permSetQuery, (error, detail) => {
-        if (error) {
-          mainWindow.webContents.send('response_generic', {
-            status: false,
-            message: 'Permission Set Detail Lookup Failed',
-            response: `${error}`,
-            limitInfo: conn.limitInfo,
-            request: args,
-          });
+      const detail = await conn.query(permSetQuery);
 
-          logMessage('Error', `Permission Set Detail Lookup Error: ${error}`);
-          return true;
-        }
+      logMessage('Info', `Fetched Permission Set details with: ${permSetQuery}`);
 
-        logMessage('Info', `Fetched Permission Set details with: ${permSetQuery}`);
-
-        // Send records back to the interface.
-        mainWindow.webContents.send('response_permset_detail', {
-          status: true,
-          message: 'Permission Set Details Complete',
-          response: detail,
-          limitInfo: conn.limitInfo,
-          request: args,
-        });
-        return true;
+      mainWindow.webContents.send('response_permset_detail', {
+        status: true,
+        message: 'Permission Set Details Complete',
+        response: detail,
+        limitInfo: conn.limitInfo,
+        request: args,
       });
-      return true;
-    });
+    } catch (err) {
+      logMessage('Error', `Permission Set Detail Lookup Error: ${err}`);
+      mainWindow.webContents.send('response_generic', {
+        status: false,
+        message: 'Permission Set Detail Lookup Failed',
+        response: `${err}`,
+        limitInfo: conn.limitInfo,
+        request: args,
+      });
+    }
   },
 };
 
